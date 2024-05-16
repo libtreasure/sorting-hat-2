097f4bccc2de6b299ed8fbb58efc9622e7361cf3
yussufshariff <yussuf.shariff@booking.com>
2024-05-16 15:01:35 +0100

    Add functionailty for employees names: Tom or Volde, wont be able to work here

--- sorting.ts
+++ sorting.ts
@@ -1,6 +1,19 @@
 const houses = ['Ridesclaw', 'Hotelpuff', 'Gryffinsure', 'Slythcarin'];
+const housesWithStudents = {
+    'ridesclaw': [],
+    'hotelpuff': [],
+    'gryffinsure': [],
+    'slythcarin': []
+};
 
 export function sortingHat(name:String) {
+const firstCharOfName = name.charAt(0)
+    if (name === 'tom' || name === 'voldemort') {
+        return 'Dark Lords are not permitted to work here!';
+    }
+    if (firstCharOfName.toLocaleLowerCase() === 'h'){
+        return`${name.charAt(0).toUpperCase() + name.slice(1)}, you are in Gryffinsure!`;
+    }
 
     let randomHouse = Math.floor(Math.random() * 4);
     return`${name.charAt(0).toUpperCase() + name.slice(1)}, you are in ${houses[randomHouse]}!`;
