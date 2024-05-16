4b6e5a36fda03adaf83184137dcdc5f56199614a
Chrissie <christinafoley@rentalcars.com>
2024-05-16 15:01:44 +0100

    Harry is now in Hotelpuff

--- houses.ts
+++ houses.ts
@@ -1,6 +1,6 @@
 export function getHouse(name : string){
     let houseArray: string [] = ["Hotelpuff", "Ridesclaw", "Gryffinsure", "Slythcarin"];
-    const randomIndex: number = Math.floor(Math.random() * houseArray.length);
+    // const randomIndex: number = Math.floor(Math.random() * houseArray.length);
     // let houseName: string | undefined = houseArray[randomIndex];
     // let nameArray: string [] = ["Harry","Tom","Ron","Ginny"];
     if (name.startsWith("H")) {
